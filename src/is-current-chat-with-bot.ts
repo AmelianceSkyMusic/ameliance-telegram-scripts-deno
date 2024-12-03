d03f5925d91e823cbc42886866302db2a9788fc4
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-12-03 19:59:26 +0200

    fix: isCurrentChatWithBot

--- src/is-current-chat-with-bot.ts
+++ src/is-current-chat-with-bot.ts
@@ -1,13 +1,5 @@
 import { Context } from '../deps.deno.ts';
 
-const BOT_USERNAME = Deno.env.get('BOT_USERNAME');
-const BOT_ID = Deno.env.get('BOT_ID');
-
 export function isCurrentChatWithBot(ctx: Context) {
-	const currentMeUsername = String(ctx.me.username);
-	const currentMeId = String(ctx.me.id);
-
-	return (
-		currentMeUsername === String(BOT_USERNAME).trim() || currentMeId === String(BOT_ID).trim()
-	);
+	return ctx.msg?.chat.type === 'private' ? true : false;
 }
