d605e88b13d57e874be0129db4c4cc8d6bc7fc5f
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-12-07 00:12:58 +0200

    chore: update submodule

--- src/run-with-random-interval.ts
+++ src/run-with-random-interval.ts
@@ -1,25 +1,30 @@
 import { getCurrentTimeWithOffset, getRandomNumber } from 'npm:ameliance-scripts';
 
+const APP_NAME = Deno.env.get('APP_NAME');
+
 import { Context } from '../deps.deno.ts';
 
 const oneMinInMs = 1000 * 60;
 
 export async function runWithRandomInterval(
 	ctx: Context,
-	callback: () => void,
+	callback: () => Promise<void>,
 	min: number,
 	max: number,
 ) {
-	callback();
+	await callback();
 
 	const randomInterval = getRandomNumber(min, max) * oneMinInMs;
 	await ctx.reply(
-		`Next post will be sent at: ${getCurrentTimeWithOffset(randomInterval)}\nPost delay: ${
-			randomInterval / oneMinInMs
-		} minutes`,
+		`<blockquote><b>ℹ️INFO: ${APP_NAME}</b></blockquote>\n<code>Next post will be sent at: ${getCurrentTimeWithOffset(
+			randomInterval,
+		)}\nPost delay: ${randomInterval / oneMinInMs} minutes</code>`,
+		{ parse_mode: 'HTML' },
 	);
 
 	setTimeout(() => {
 		runWithRandomInterval(ctx, callback, min, max);
 	}, randomInterval);
+
+	return randomInterval;
 }
