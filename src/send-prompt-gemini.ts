f316fcb4d07c71835991b92d65151ddb70baeb30
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2025-02-28 09:16:24 +0200

    chore: remove try catch in sendPromptGemini

--- src/send-prompt-gemini.ts
+++ src/send-prompt-gemini.ts
@@ -4,15 +4,11 @@ const GOOGLE_GEMINI_API = Deno.env.get('GOOGLE_GEMINI_API');
 
 export async function sendPromptGemini(prompt: string) {
 	if (!GOOGLE_GEMINI_API) throw new Error('GOOGLE_GEMINI_API is missing!');
-	try {
-		const genAI = new GoogleGenerativeAI(GOOGLE_GEMINI_API);
-		const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
-		const result = await model.generateContent(prompt);
-		const response = await result.response;
-		const text = response.text();
-		return text;
-	} catch (error) {
-		console.error('Gemini API Error:', error);
-		if (error instanceof Error) throw new Error(`Failed to generate content: ${error.message}`);
-	}
+
+	const genAI = new GoogleGenerativeAI(GOOGLE_GEMINI_API);
+	const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+	const result = await model.generateContent(prompt);
+	const response = await result.response;
+	const text = response.text();
+	return text;
 }
