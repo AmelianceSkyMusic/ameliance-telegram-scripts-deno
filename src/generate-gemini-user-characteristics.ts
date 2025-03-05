d701e2580dcff0638ce53076c214746962f04326
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2025-03-05 16:12:35 +0200

    refactor: fix potential error generateGeminiUserCharacteristics

--- src/generate-gemini-user-characteristics.ts
+++ src/generate-gemini-user-characteristics.ts
@@ -2,6 +2,6 @@ export function generateGeminiUserCharacteristics(
 	username: string,
 	getCharacteristics: (username: string) => string | undefined,
 ) {
-	const userCharacteristics = getCharacteristics(username.toLocaleLowerCase());
+	const userCharacteristics = getCharacteristics(username?.toLocaleLowerCase() || '');
 	return userCharacteristics ? `{USER_CHARACTERISTICS_TO_CONSIDER: ${userCharacteristics}}\n` : '';
 }
