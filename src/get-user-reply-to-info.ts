db6926bb682da191c770fb540ae6f9f5e31f7016
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2025-03-05 17:54:26 +0200

    chore: BOT_USERNAME to getUserReplyToInfo

--- src/get-user-reply-to-info.ts
+++ src/get-user-reply-to-info.ts
@@ -3,12 +3,17 @@ import { getHTMLData } from './get-html-data.ts';
 import { getLinksFromMessage } from './get-links-from-message.ts';
 import { getTextFromHTML } from './get-text-form-html.ts';
 
+const BOT_USERNAME = Deno.env.get('BOT_USERNAME');
+
+if (BOT_USERNAME) console.log('BOT_USERNAME is not set');
+
 export function getUserReplyToInfo(ctx: Context) {
 	const context = ctx.msg.reply_to_message;
 	if (!context) return null;
 	return {
 		context,
 		isBot: context?.from?.is_bot,
+		isThisBot: BOT_USERNAME === context?.from?.username,
 		lastName: context.from?.last_name,
 		username: context.from?.is_bot ? 'bot' : context.from?.username,
 		fullName: [context.from?.first_name, context.from?.last_name].join(' ').trim(),
