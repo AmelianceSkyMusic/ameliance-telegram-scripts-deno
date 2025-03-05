44deba025814e32a26839c7685ca4b5b1b921838
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2025-03-05 15:26:22 +0200

    chore: update deps.deno

--- deps.deno.ts
+++ deps.deno.ts
@@ -4,3 +4,8 @@ export type { RawApi } from 'https://deno.land/x/grammy@v1.32.0/mod.ts';
 
 export type { Message } from 'https://deno.land/x/grammy_types@v3.16.0/message.ts';
 export type { Other } from 'https://deno.land/x/grammy@v1.32.0/core/api.ts';
+
+export type {
+	CallbackError,
+	Connection,
+} from 'https://deno.land/x/mongoose@8.8.2/types/models.d.ts';
