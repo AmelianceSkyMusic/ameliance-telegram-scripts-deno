f17887c38558b82313dcb64224f3bcbb27413129
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2025-03-05 15:25:08 +0200

    refactor: getUserInfo

--- src/get-user-info.ts
+++ src/get-user-info.ts
@@ -1,12 +1,33 @@
 import { Context } from '../deps.deno.ts';
+import { getHTMLData } from './get-html-data.ts';
+import { getLinksFromMessage } from './get-links-from-message.ts';
+import { getTextFromHTML } from './get-text-form-html.ts';
 
 export function getUserInfo(ctx: Context) {
+	const context = ctx.msg;
 	return {
-		firstName: ctx.from?.first_name,
-		lastName: ctx.from?.last_name,
-		fullName: ctx.from?.username,
-		username: ctx.from?.username,
-		fullname: [ctx.from?.first_name, ctx.from?.last_name].join(' ').trim(),
-		userId: ctx.from?.id,
+		context,
+		userId: context.from?.id,
+		username: context.from?.username,
+		firstName: context.from?.first_name,
+		lastName: context.from?.last_name,
+		fullName: [context.from?.first_name, context.from?.last_name].join(' ').trim(),
+		message: context.text,
+		messageId: context.message_id,
+		chatType: context.chat.type,
+		date: context.date,
+		async getTextFromUrl() {
+			let textFromUrlInCurrentMessage = '';
+			const currentTextHTMLUrls = getLinksFromMessage(context);
+
+			if (currentTextHTMLUrls && currentTextHTMLUrls?.length > 0) {
+				const url = currentTextHTMLUrls[0];
+				const dataFromHTML = await getHTMLData(ctx, url);
+				if (dataFromHTML) {
+					textFromUrlInCurrentMessage = getTextFromHTML(dataFromHTML);
+				}
+			}
+			return textFromUrlInCurrentMessage;
+		},
 	};
 }
