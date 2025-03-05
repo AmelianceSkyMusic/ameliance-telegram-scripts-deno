2cf517b09a208af1e1f3a4c4b0ba58655c41dadd
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2025-03-05 15:24:37 +0200

    feat: add generateGeminiUserCharacteristics

---
+++ src/generate-gemini-user-characteristics.ts
@@ -0,0 +1,7 @@
+export function generateGeminiUserCharacteristics(
+	username: string,
+	getCharacteristics: (username: string) => string | undefined,
+) {
+	const userCharacteristics = getCharacteristics(username.toLocaleLowerCase());
+	return userCharacteristics ? `{USER_CHARACTERISTICS_TO_CONSIDER: ${userCharacteristics}}\n` : '';
+}
