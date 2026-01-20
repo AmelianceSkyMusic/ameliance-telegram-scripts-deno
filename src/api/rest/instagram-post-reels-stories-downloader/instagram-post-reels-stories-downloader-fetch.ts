91e5387cfd6cb6397d796fe24d7ae1a2b685898c
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-01-20 16:12:39 +0200

    refactor: update instagram-post-reels-stories-downloader api

--- src/api/rest/instagram-post-reels-stories-downloader/instagram-post-reels-stories-downloader-fetch.ts
+++ src/api/rest/instagram-post-reels-stories-downloader/instagram-post-reels-stories-downloader-fetch.ts
@@ -6,9 +6,9 @@ const RAPID_API_KEY = Deno.env.get('RAPID_API_KEY');
 if (!APP_NAME) console.error('APP_NAME is not set');
 if (!RAPID_API_KEY) console.error('RAPID_API_KEY is not set');
 
-const url = 'https://instagram-post-reels-stories-downloader.p.rapidapi.com/instagram/';
+const url = 'https://instagram-post-reels-stories-downloader-api.p.rapidapi.com/instagram/';
 const headers = {
-	'X-RapidAPI-Host': 'instagram-post-reels-stories-downloader.p.rapidapi.com',
+	'X-RapidAPI-Host': 'instagram-post-reels-stories-downloader-api.p.rapidapi.com',
 	'X-RapidAPI-Key': RAPID_API_KEY || '',
 };
 
